ee7034869995b1ee1ad6bcf5fd8d4437a8e59183
Nick Bystrov <projectuniver@yandex.ru>
2025-11-17 20:13:44 +0300

    start test

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
 import './scss/styles.scss';
+import { Gallery } from './components/Models/Gallery';
+import { Basket } from './components/Models/Basket';
+import { Buyer } from './components/Models/Buyer';
+import { IBuyer, IProduct } from './types';
+import { apiProducts } from './utils/data'
+
+
+
+const gallery = new Gallery();
